b0c62303a62acd521bc8e28ffc9912219a198908
erkinchik <erkin.berdibaev@gmail.com>
2022-07-14 15:49:49 +0600

    add ocntainer

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -2,11 +2,17 @@ import React from "react";
 
 import { ReactComponent as LogoIcon } from "../../assets/icons/logoIcon.svg";
 
+import classes from "./Header.module.scss";
+
 const Header = () => {
   return (
-    <header>
-      <div>
-        <LogoIcon />
+    <header className={classes.header}>
+      <div className={classes.container}>
+        <div className={classes.headerLogo}>
+          <i>
+            <LogoIcon />
+          </i>
+        </div>
       </div>
     </header>
   );
