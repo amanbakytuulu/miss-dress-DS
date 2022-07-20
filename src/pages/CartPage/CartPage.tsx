31f6d16e75cb03efd14bb1f14b6b88ee8acaed55
erkinchik <erkin.berdibaev@gmail.com>
2022-07-20 19:15:42 +0600

    MID-27 add fixes

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -21,23 +21,20 @@ const arr: ICartList[] = [
 const CartPage = () => {
   return (
     <div className={classes.cartPage}>
-      <div className={classes.container}>
-        <div className={classes.cartPageHeader}>
+      <div className={`${classes.cartPageWrapper} ${classes.container}`}>
+        <div className={classes.cartPageMain}>
           <div className={classes.cartPageOrder}>
             <OrderForm />
           </div>
-
-          <div className={classes.cartPageSum}>
-            <CartSummary />
-          </div>
-        </div>
-        <div className={classes.cartPageListBlock}>
           <h3 className={classes.cartPageListTitle}>Состав заказа</h3>
           <div className={classes.cartPageList}>
             {arr.map((item) => {
               return <CartItem />;
             })}
           </div>
+        </div>
+        <div className={classes.cartPageSum}>
+          <CartSummary />
         </div>
       </div>
     </div>
