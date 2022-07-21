dd3f08e64aa7485b8eb2c9b59e728e742df15169
Matvey <matvey55561@mail.ru>
2022-07-21 13:22:19 +0600

    MID-12 fix--subscrube

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,14 @@ import React from "react";
 
 import { AppRouter } from "./components";
 
-import ColactionContainer from "./pages/MainPage/components/CollactionContainer/CollactionContainer";
-import SubscrubeContainer from "./pages/MainPage/components/SubscrubeContainer/SubscrubeContainer";
 import { Footer, Header } from "./layout";
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
-      {/* <AppRouter /> */}
-      <ColactionContainer />
-      <SubscrubeContainer />
-      {/* <Footer /> */}
+      <Header />
+      <AppRouter />
+      <Footer />
     </div>
   );
 }
