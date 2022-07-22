6a29c9dc4cd699175257f80e3efdde30f6e5156e
Matvey <matvey55561@mail.ru>
2022-07-22 13:31:39 +0600

    MID-12 -delivery

--- src/pages/DeliveryPage/DeliveryPage.tsx
+++ src/pages/DeliveryPage/DeliveryPage.tsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+import classes from "../DeliveryPage/DeliveryPage.module.scss";
+
+import DeliveryComponents from "./DeliveryComponents/DeliveryComponents";
+
 const DeliveryPage = () => {
-  return <div>DELIVERY</div>;
+  return (
+    <>
+      <div className={classes.deliv}>
+        <DeliveryComponents />
+        <div className={classes.flex}>
+          <div className={classes.deliv__item}>
+            <h1>Доставка</h1>
+            <p>
+              После того, как Вы сделали заказ, наши менеджеры связываются с
+              Вами для подтверждения заказа (обработка заказов происходит с
+              понедельника по пятницу с 10:00 до 20:00)
+            </p>
+            <p>Заказ без оплаты остается в резерве 3 рабочих дня. </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DeliveryPage;
--- src/pages/MainPage/components/SubscrubeContainer/SubscrubeContainer.tsx
+++ src/pages/MainPage/components/SubscrubeContainer/SubscrubeContainer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 import classes from "../SubscrubeContainer/SubscrubeContainer.module.scss";
 // import Fonsub from "../../../../assets/Main/Subscrube/fonsub.png";
