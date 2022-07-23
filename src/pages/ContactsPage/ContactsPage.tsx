cf9a34b0e84810e978de23df08926066f2a0b2f0
erkinchik <erkin.berdibaev@gmail.com>
2022-07-23 18:23:56 +0600

    MID-30 add contacts page

--- src/pages/ContactsPage/ContactsPage.tsx
+++ src/pages/ContactsPage/ContactsPage.tsx
@@ -1,7 +1,52 @@
 import React from "react";
 
+import { ReactComponent as WhatsAppIcon } from "../../assets/icons/contactIcons/whatsappIcon.svg";
+import { ReactComponent as TelegramIcon } from "../../assets/icons/contactIcons/telegramIcon.svg";
+import { ReactComponent as InstagramIcon } from "../../assets/icons/contactIcons/instagramIcon.svg";
+
+import { ISocialTypes } from "../../types/SocialTypes/SocialTypes";
+
+import SocialList from "./SocialList/SocialList";
+
+import classes from "./ContactsPage.module.scss";
+
 const ContactsPage = () => {
-  return <div>CONTACTS</div>;
+  const socials: ISocialTypes[] = [
+    {
+      icon: <WhatsAppIcon />,
+      contacts: "+996712345678",
+    },
+    {
+      icon: <TelegramIcon />,
+      contacts: "+996712345678",
+    },
+    {
+      icon: <InstagramIcon />,
+      contacts: "@missdress",
+    },
+  ];
+
+  return (
+    <div className={classes.contactsWrapper}>
+      <h2 className={classes.contactsTitle}>Контакты</h2>
+
+      <div className={classes.phoneNumber}>
+        <span>Тел: +996712345678, +996787654321</span>
+      </div>
+
+      <SocialList socials={socials} />
+
+      <div className={classes.email}>
+        <span>Email: missdress@gmail.com</span>
+      </div>
+
+      <div className={classes.workingHours}>
+        <span>
+          Рабочие дни: Пн. - Пт: с 10:00 до 20:00 <br /> Выходные дни: Сб-Вс
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default ContactsPage;
