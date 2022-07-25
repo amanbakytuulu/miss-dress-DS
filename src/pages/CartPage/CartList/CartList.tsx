57351ff03e1022375a6f9fe8f3b1d13b98300220
erkinchik <erkin.berdibaev@gmail.com>
2022-07-25 16:57:10 +0600

    MID-27 add fixes with OrderForm

---
+++ src/pages/CartPage/CartList/CartList.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from "react";
+
+import CartItem from "../../../components/CartItem/CartItem";
+
+import classes from "./CartList.module.scss";
+
+interface CartListProps {
+  cartList: any;
+}
+
+const CartList: FC<CartListProps> = ({ cartList }) => {
+  return (
+    <div className={classes.cartPageList}>
+      {cartList.map((item: any) => {
+        return (
+          <div className={classes.cartItem}>
+            <CartItem />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default CartList;
